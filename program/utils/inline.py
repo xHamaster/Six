f2c04f457469c39314aacc588297fe94354ecb5b
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-11 21:55:34 +0530

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -11,8 +11,7 @@ from pyrogram.types import (
 def stream_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data=f'stream_menu_panel | {user_id}'),
-      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data=f'set_close'),
+      InlineKeyboardButton(text="ᴄʟᴏꜱᴇ", callback_data=f'set_close'),
     ],
   ]
   return buttons
