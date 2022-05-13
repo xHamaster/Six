800c9699c5fde8fc0ea87dbaf9fc4dcbfc311dab
xHamaster <105044443+xHamaster@users.noreply.github.com>
2022-05-13 20:57:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-from py-tgcalls import idle
 from driver.core import calls, bot, user
 
 
@@ -8,11 +7,8 @@ async def start_bot():
     print("[INFO]: BOT & UBOT CLIENT STARTED !!")
     await calls.start()
     print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
-    await user.join_chat("VeezSupportGroup")
-    await user.join_chat("levinachannel")
-    await idle()
-    print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.stop()
-
+    await user.join_chat("TeamCodexun")
+    await user.join_chat("Codexun")
+    
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_bot())
